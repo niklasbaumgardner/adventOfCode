438f47bd12b3676c8297a6ad4cdc05182da9c49a
niklasbaumgardner <niklasbaumgardner@gmail.com>
2024-12-17 16:18:52 -0500

    day 16 part 2 attempt

--- 2024/16.py
+++ 2024/16.py
@@ -4,6 +4,12 @@ from helpers import read_file, Matrix, Node, Point
 
 
 MOVEMENT = {"N": Point(0, -1), "E": Point(1, 0), "S": Point(0, 1), "W": Point(-1, 0)}
+POINT_TO_DIR = {
+    Point(0, -1): "N",
+    Point(1, 0): "E",
+    Point(0, 1): "S",
+    Point(-1, 0): "W",
+}
 
 
 PATH = Path(__file__)
@@ -75,21 +81,68 @@ class WeightedMatrix(Matrix):
                 node.down = self.at(node.point.x, node.point.y + 1)
 
     def find_best_path(self):
-        paths = dict()
+        self.paths = dict()
         for row in self.matrix:
             for n in row:
                 if n.value != "#":
-                    paths[n.point] = (999999999, None)
+                    for dir in ["N", "S", "E", "W"]:
 
-        paths[self.start.point] = (0, None)
+                        self.paths[n.point] = (999999999, None)
+
         starting_dir = "E"
+        self.paths[self.start.point] = (0, None)
         queue = [(self.start, 0, starting_dir)]
 
         visited = set()
 
         while queue:
             (node, current_weight, dir) = queue.pop(0)
-            current_weight = paths[node.point][0]
+            # current_weight = self.paths[node.point][0]
+
+            visited.add(node.point)
+
+            neighbors = node.get_valid_neighbors(dir)
+            # print(node, neighbors)
+            # return
+            for n, weight, n_dir in neighbors:
+                if n.point not in visited:
+                    new_weight = current_weight + weight
+                    if new_weight < self.paths[n.point][0]:
+                        self.paths[n.point] = (new_weight, node)
+
+                        queue.append((n, new_weight, n_dir))
+
+            queue = sorted(queue, key=lambda x: x[1])
+
+            visited.add(n.point)
+        # print(self.paths)
+        # min_path = (9999999999999999, "")
+        # for dir in ["N", "S", "E", "W"]:
+        #     path_len = self.paths[(self.end.point, dir)][0]
+        #     if path_len < min_path[0]:
+        #         min_path = (path_len, dir)
+
+        print(len(visited))
+        return self.paths[self.end.point][0]
+
+    def find_best_path_v2(self):
+        self.paths_with_dir = dict()
+        for row in self.matrix:
+            for n in row:
+                if n.value != "#":
+                    for dir in ["N", "S", "E", "W"]:
+
+                        self.paths_with_dir[(n.point, dir)] = (999999999, None)
+
+        starting_dir = "E"
+        self.paths_with_dir[(self.start.point, starting_dir)] = (0, None)
+        queue = [(self.start, 0, starting_dir)]
+
+        visited = set()
+
+        while queue:
+            (node, current_weight, dir) = queue.pop(0)
+            # current_weight = self.paths[node.point][0]
 
             # visited.add((node.point, dir))
 
@@ -98,16 +151,190 @@ class WeightedMatrix(Matrix):
             # return
             for n, weight, n_dir in neighbors:
                 if (n.point, dir) not in visited:
-                    new_weight = paths[node.point][0] + weight
-                    if new_weight < paths[n.point][0]:
-                        paths[n.point] = (new_weight, node)
+                    new_weight = current_weight + weight
+                    if new_weight < self.paths_with_dir[(n.point, n_dir)][0]:
+                        self.paths_with_dir[(n.point, n_dir)] = (new_weight, node)
 
                         queue.append((n, new_weight, n_dir))
 
             queue = sorted(queue, key=lambda x: x[1])
 
             # visited.add(n.point)
-        return paths[self.end.point][0]
+        # print(self.paths)
+        min_path = (9999999999999999, "")
+        for dir in ["N", "S", "E", "W"]:
+            path_len = self.paths_with_dir[(self.end.point, dir)][0]
+            print(dir, self.paths_with_dir[(self.end.point, dir)])
+            if path_len < min_path[0]:
+                min_path = (path_len, dir)
+
+        print(min_path)
+        return min_path[0]
+
+        # return self.paths[self.end.point][0]
+
+    def find_best_path_v3(self):
+        self.paths = dict()
+        dist = dict()
+        prev = dict()
+        for row in self.matrix:
+            for n in row:
+                if n.value != "#":
+                    self.paths[n.point] = [(999999999, None)]
+                    dist[n.point] = 999999999
+                    prev[n.point] = None
+
+        starting_dir = "E"
+        self.paths[self.start.point] = [(0, None)]
+        dist[self.start.point] = 0
+        queue = [(self.start, 0, starting_dir)]
+
+        visited = set()
+
+        while queue:
+            (node, current_weight, dir) = queue.pop(0)
+            # current_weight = self.paths[node.point][0]
+            if node.point in visited:
+                continue
+
+            neighbors = node.get_valid_neighbors(dir)
+            # print(node, neighbors)
+            # return
+            for n, weight, n_dir in neighbors:
+                # if n.point not in visited:
+                # if n != node:
+                new_weight = current_weight + weight
+                current_weight_to_node = self.paths[n.point][0][0]
+
+                if self.paths[n.point][0][1] is None:
+                    self.paths[n.point].pop(0)
+
+                self.paths[n.point].append((new_weight, node))
+                queue.append((n, new_weight, n_dir))
+
+                # if new_weight < current_weight_to_node:
+                #     # self.paths[n.point].pop(0)
+                #     if self.paths[n.point][0][1] is None:
+                #         self.paths[n.point].pop(0)
+                #     self.paths[n.point].append((new_weight, node))
+                #     queue.append((n, new_weight, n_dir))
+                # elif new_weight == current_weight_to_node:
+                #     self.paths[n.point].append((new_weight, node))
+                #     queue.append((n, new_weight, n_dir))
+
+            visited.add(node.point)
+            queue = sorted(queue, key=lambda x: x[1])
+
+        # print(self.paths)
+        # for k, v in self.paths.items():
+        #     print(k, v)
+        # min_path = (9999999999999999, "")
+        # for dir in ["N", "S", "E", "W"]:
+        #     path_len = self.paths[(self.end.point, dir)][0]
+        #     if path_len < min_path[0]:
+        #         min_path = (path_len, dir)
+
+        # print(len(visited))
+        return self.paths[self.end.point][0]
+
+    def can_node_reach_end_under_best_path_weight(
+        self, start, start_dir, best_path_weight
+    ):
+        if start == self.end.point:
+            return True
+
+        q = [(self.paths[start][0][0], start, start_dir)]
+
+        while q:
+            w, node, dir = q.pop(0)
+
+            if w > best_path_weight:
+                return False
+
+            neighbors = node.get_valid_neighbors(dir)
+            # print(node, neighbors)
+            # return
+            for n, weight, n_dir in neighbors:
+                new_weight = w + weight
+                if new_weight < best_path_weight:
+                    q.append((self.paths[n.point][0][0], n.point, n_dir))
+
+            # node = self.
+            # [node.point][1]
+            q = sorted(q, key=lambda x: x[0])
+        return True
+
+    def find_nodes_along_path_v4(self, best_path_weight):
+        nodes = []
+        for k, v in self.paths.items():
+            for w, n in v:
+                dir = POINT_TO_DIR[k - n.point]
+                nodes.append((w, n, dir))
+        nodes = sorted(nodes, key=lambda x: x[0])
+
+        points = set()
+
+        for _, node, dir in nodes:
+            if self.can_node_reach_end_under_best_path_weight(
+                node.point, best_path_weight, dir
+            ):
+                points.add(node.point)
+
+        for p in points:
+            self.at_point(p).value = "O"
+
+        return len(points)
+
+    def find_nodes_along_path_v3(self, best_path_weight):
+        path = set()
+        nodes = [(0, self.end)]
+        # for k, v in self.paths.items():
+        #     print(k, v)
+
+        # for k, v in self.paths.items():
+        #     for t in v:
+        #         nodes.append(t)
+        # nodes = sorted(nodes, key=lambda x: x[0])
+        # print(nodes)
+        visited = set()
+
+        while nodes:
+            weight, node = nodes.pop(0)
+            if not node:
+                continue
+
+            # if node.point in visited:
+            #     continue
+
+            if node == self.start:
+                print("HIT STARTING POINT", len(path))
+
+            path.add(node.point)
+
+            ns = self.paths[node.point]
+            for w, n in ns:
+                print(w, n)
+                if n.point not in visited and w <= best_path_weight:
+                    # if n == self.start:
+                    #     print(len(path), path)
+                    #     for p in path:
+                    #         self.at_point(p).value = "O"
+                    #     return
+                    nodes.append((w, n))
+                    visited.add(n.point)
+
+            # node = self.
+            # [node.point][1]
+            nodes = sorted(nodes, key=lambda x: x[0])
+            print(nodes)
+
+        print(len(path), path)
+        print(self.paths[self.end.point])
+
+        for p in path:
+            self.at_point(p).value = "O"
+
+        return len(path)
 
 
 def part1():
@@ -120,11 +347,19 @@ def part1():
 
 
 def part2():
-    return
+    m = WeightedMatrix(DATA)
+    print(m)
+    weight = m.find_best_path_v3()
+    print("done", weight)
+
+    length = m.find_nodes_along_path_v4(weight[0])
+    print(m)
+
+    return length
 
 
 def main():
-    print(f"Part 1: {part1()}")
+    # print(f"Part 1: {part1()}")
     print(f"Part 2: {part2()}")
 
 
