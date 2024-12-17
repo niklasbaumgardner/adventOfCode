04aacedf3a2306813f2640300909537e11491145
niklasbaumgardner <niklasbaumgardner@gmail.com>
2024-12-17 11:12:11 -0500

    day 16 part 1 complete

--- 2024/16.py
+++ 2024/16.py
@@ -4,8 +4,10 @@ from helpers import read_file, Matrix, Node, Point
 
 
 MOVEMENT = {"N": Point(0, -1), "E": Point(1, 0), "S": Point(0, 1), "W": Point(-1, 0)}
+
+
 PATH = Path(__file__)
-YEAR = str(PATH).split("/")[-2]
+YEAR = str(PATH).split("\\")[-2]
 DATA = read_file(f"./{YEAR}/{PATH.name.split('.')[0]}.txt")
 
 
@@ -43,7 +45,7 @@ class WeightedNode(Node):
         if self.down and self.down.value != "#":
             neighbors.append([self.down, 1001 ** abs(dir_point.x), "S"])
         if self.left and self.left.value != "#":
-            neighbors.append([self.left, 1001 ** abs(dir_point.x), "W"])
+            neighbors.append([self.left, 1001 ** abs(dir_point.y), "W"])
 
         return neighbors
 
@@ -89,10 +91,6 @@ class WeightedMatrix(Matrix):
             (node, current_weight, dir) = queue.pop(0)
             current_weight = paths[node.point][0]
 
-            # if node == self.end:
-            #     print(queue)
-            #     return current_weight
-
             # visited.add((node.point, dir))
 
             neighbors = node.get_valid_neighbors(dir)
@@ -107,8 +105,6 @@ class WeightedMatrix(Matrix):
                         queue.append((n, new_weight, n_dir))
 
             queue = sorted(queue, key=lambda x: x[1])
-            print(queue)
-            return
 
             # visited.add(n.point)
         return paths[self.end.point][0]
